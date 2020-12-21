a4dbe194aa027f529bb014dd746fe74bcf7624b0
Jess Xu <jsxu315@gmail.com>
2020-12-20 22:31:22 -0800

    basic enemy movement

--- Assets/Members/Jess/Bullet.cs
+++ Assets/Members/Jess/Bullet.cs
@@ -6,27 +6,39 @@ public class Bullet:MonoBehaviour
 {
     public float horizontal; 
     public float vertical; 
-    const int SPEED = 2; 
+    const int SPEED = 3; 
+    public GameObject Player;    
     
-    /*
-    void Start()
+    /*void Start()
     { 
         //for testing purposes
         horizontal = 1; 
         vertical = 1; 
-    } */ 
+    } */
 
     void Update()
     {
         //move the bullet forward
-        
         float delta_x = horizontal * Time.deltaTime * SPEED; 
         float delta_y = vertical * Time.deltaTime * SPEED; 
         this.transform.position = new Vector2(transform.position.x + delta_x, transform.position.y + delta_y);
+
     }
 
-    //todo: if it hits the player, destroy the bullet and remove some health? 
+    //todo: if it hits the player, decrease player health 
+    void OnTriggerEnter2D()
+    {
+        //Debug.Log("collision!");
+        Destroy(this.gameObject);
+    }
 
-    //todo: destroy the bullet if it flies off screen 
+    //Destroy the bullet if it flies off screen 
+    //Kind of redundant because it also gets called when OnTriggerEnter2D gets called
+    void OnBecameInvisible()
+    {
+        //Debug.Log("Destroyed off-screen bullet");
+        Destroy(this.gameObject);
+    }
+
 
 }
--- Assets/Members/Jess/Enemy.cs
+++ Assets/Members/Jess/Enemy.cs
@@ -4,32 +4,40 @@ using UnityEngine;
 
 public class Enemy:MonoBehaviour
 {
-    private GameObject EnemyObject; 
+    const float WAIT_TIME = 1.0f; //fires bullets at 1 second intervals 
+    //bounds for spawning enemies in; maybe use camera instead?
+    const float LEFT_BOUND = 0f; 
+    const float RIGHT_BOUND = 5f; 
+    const float LOWER_BOUND = -5f; 
+    const float UPPER_BOUND = 5f; 
+    const float MOVE_SPEED = 1.0f; 
     public GameObject BulletObject; //a prefab 
     private Vector2 EnemyPosition; 
-    public GameObject Player; //follow the Player using tags? 
-    const float WAIT_TIME = 1.0f; //fires bullets at 1 second intervals 
-    float time_elapsed = 0;
+    public GameObject Player; 
+    private float time_elapsed = 0;
+    private bool CR_running = false; 
 
     void Start()
     {
         //get the Player 
         Player = GameObject.FindWithTag("Player"); 
 
-        //instantiate enemy at random position 
-        EnemyPosition = new Vector2(Random.Range(-100F, 100F), Random.Range(-100F, 100F));
-        if(EnemyPosition != (Vector2)Player.transform.position) //don't spawn an enemy on top of a player
+        //Spawn enemy at random position 
+        Vector2 temp = new Vector2(Random.Range(LEFT_BOUND, RIGHT_BOUND), Random.Range(LOWER_BOUND, UPPER_BOUND));
+        if(temp != (Vector2)Player.transform.position) //don't spawn an enemy on top of a player
         {
-            Instantiate(EnemyObject, EnemyPosition, Quaternion.identity); 
+            transform.position = temp; 
+            StartCoroutine(Move());
         }
-        
-        
     }
 
     void Update()
     {
         //move in a random direction, somewhat following the player? 
-        //just stationary for now 
+        if(CR_running == false)
+        {
+            StartCoroutine(Move());
+        }
 
         time_elapsed += Time.deltaTime; 
         if(time_elapsed >= WAIT_TIME)
@@ -43,18 +51,124 @@ public class Enemy:MonoBehaviour
     void Shoot()
     {
         //calculate the path the bullet should go on 
-        Vector2 player_position = Player.transform.position; 
-        float h = this.transform.position.x - player_position.x; 
-        float v = this.transform.position.y - player_position.y;
-        float magnitude = Mathf.Sqrt((h*h) + (v*v));
-        h /= magnitude; 
-        v /= magnitude; 
-
+        float h = 1000; //really big placeholder values for debugging
+        float v = 1000; 
+        getPlayerVector(ref h, ref v, Player.transform.position);
 
         GameObject temp = Instantiate(BulletObject, (Vector2)transform.position, Quaternion.identity); 
         Bullet b = temp.GetComponent<Bullet> (); 
-        b.transform.parent = this.transform; //make the bullet a child of the enemy
+        //b.transform.parent = this.transform; //make the bullet a child of the enemy
         b.horizontal = h; 
         b.vertical = v; 
+    }
+
+    IEnumerator Move() //unfinished
+    {
+        CR_running = true; 
+        
+        //move in a random direction, but don't get too close to the player
+        //have enemies move within a predetermined box rn for convenience 
+        //todo: prevent enemies from running into each other
+
+        //const float dist_between_enemies = 1.0f; 
+        const float min_distance_from_player = 3.0f; 
+        Vector2 player_pos = Player.transform.position; 
+        float upper_bound = player_pos.y + min_distance_from_player; //should rename later 
+        float lower_bound = player_pos.y - min_distance_from_player; 
+        float right_bound = player_pos.x + min_distance_from_player;
+        float left_bound = player_pos.x - min_distance_from_player; 
+
+        float delta_x = 0; 
+        float delta_y = 0; 
+        int dir = Random.Range(0, 4); 
+
+        
+        if(dir == 0 || dir == 1)
+            delta_x = 1 * Time.deltaTime * MOVE_SPEED; 
+        else if (dir == 2 || dir == 3)
+            delta_y = 1 * Time.deltaTime * MOVE_SPEED; 
+
+        //don't move if you go inside the player's "bubble"
+        if(transform.position.x + delta_x <= right_bound && transform.position.x - delta_x >= left_bound
+        && transform.position.y + delta_y <= upper_bound && transform.position.y - delta_y >= lower_bound)
+        {
+            Debug.Log("Can't move into player bubble");
+            CR_running = false;
+            yield break; 
+        }
+        
+        //also prevent the enemy from moving outside the bounds of the game 
+        if(transform.position.x + delta_x > RIGHT_BOUND || transform.position.x - delta_x < LEFT_BOUND
+        || transform.position.y + delta_y > UPPER_BOUND || transform.position.y - delta_y < LOWER_BOUND)
+        {
+            Debug.Log("Can't move outside of enemy bubble");
+            CR_running = false;
+            yield break; 
+        }
+
+        //Moves the enemy in a random direction
+        switch (dir)
+        {
+            case 0: //right 
+                Debug.Log("Moving right");
+                for(int i = 0; i < 150; i++)
+                {
+                    if(transform.position.x + delta_x < RIGHT_BOUND)
+                        transform.position = new Vector2(transform.position.x + delta_x, transform.position.y);
+
+                    yield return null; 
+                }
+                    
+                break; 
+
+            case 1: //left
+                Debug.Log("Moving left");
+                for(int i = 0; i < 150; i++){
+                    if(transform.position.x + delta_x > LEFT_BOUND)
+                        transform.position = new Vector2(transform.position.x - delta_x, transform.position.y);
+
+                    yield return null; 
+                }
+                    
+                break; 
+            
+            case 2: //up
+                Debug.Log("Moving up");
+                for(int i = 0; i < 150; i++){
+                    if(transform.position.y + delta_y < UPPER_BOUND)
+                        transform.position = new Vector2(transform.position.x, transform.position.y + delta_y);
+
+                    yield return null; 
+                }
+                    
+                break; 
+
+            case 3: //down
+                Debug.Log("Moving down");
+                for(int i = 0; i < 150; i++){
+                    if(transform.position.y + delta_y > LOWER_BOUND)
+                        transform.position = new Vector2(transform.position.x, transform.position.y - delta_y);
+
+                    yield return null; 
+                }
+
+                break; 
+            default: 
+                CR_running = false;
+                yield break; 
+        }
+        CR_running = false;
+
+    }
+
+
+
+    void getPlayerVector(ref float horizontal, ref float vertical, Vector2 player_pos)
+    {
+        horizontal = player_pos.x - this.transform.position.x ; 
+        vertical = player_pos.y - this.transform.position.y;
+        float magnitude = Mathf.Sqrt((horizontal*horizontal) + (vertical*vertical));
+        horizontal /= magnitude; 
+        vertical /= magnitude; 
     }
 }
