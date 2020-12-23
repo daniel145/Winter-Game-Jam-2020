4646a4c1d90e3faac7252dd308bdb92336f6b6cd
Daniel Park <luckynumberseven07@gmail.com>
2020-12-22 19:48:02 -0800

    Changed animation rules

---
+++ Assets/Members/Daniel/DanielScripts/AnimationAid.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationAid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator animator = GetComponent<Animator>();
+        Vector2 colliderSize = transform.parent.GetComponent<BoxCollider2D>().size;
+        float colliderWidth = colliderSize.x;
+        transform.position -= new Vector3(colliderWidth / 2, colliderSize.y / 2, 0);
+    }
+}
